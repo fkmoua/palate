71a2499b284095bfe1cf55108914c3cad3375f3f
fkmoua <mouafancy@gmail.com>
2015-07-09 01:05:20 -0700

    redirect signup/signin to current_user

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,4 @@
 class Restaurant < ActiveRecord::Base
 
-	before_action :authenticate_user!
+	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,12 @@ Rails.application.routes.draw do
   get 'users/show'
 
   devise_for :users, controllers: { registrations: "registrations" }
-  root 'welcome#index' 
+  root 'welcome#index', controllers: {registrations: "registrations"} 
 
   resources :users do 
   		resources :hitlists
   end
-  
+
   resources :restaurants
   resources :hitlist
 
