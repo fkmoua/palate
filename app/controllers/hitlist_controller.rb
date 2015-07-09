38f24e878065eb288a99a362dc5026ab1ebccf33
fkmoua <mouafancy@gmail.com>
2015-07-08 19:43:58 -0700

    signin/signout

--- app/controllers/hitlist_controller.rb
+++ app/controllers/hitlist_controller.rb
@@ -1,2 +1,20 @@
 class HitlistController < ApplicationController
+
+	def index
+	end
+
+	def show
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def update
+	end
+
+	def delete
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   get 'users/show'
 
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations" }
   root 'welcome#index' 
 
   resources :users
