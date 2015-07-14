fae544f1d03de706bc5006182fe760db7cdbbdcb
fkmoua <mouafancy@gmail.com>
2015-07-13 19:28:30 -0700

    add container class to signup/signin show page; added strong parameter for name in User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
 
-  root 'welcome#index' 
+  get 'users/index'
+
+  get 'users/show'
+
+  devise_for :users, controllers: { registrations: "registrations" }
+  root 'welcome#index', controllers: {registrations: "registrations"} 
+
+  resources :users do 
+  		resources :hitlists
+  end
 
   resources :restaurants
   resources :hitlist
